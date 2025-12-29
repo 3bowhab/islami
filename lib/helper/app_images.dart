30f5e6f9aef2096948af5d7b4569a6ccc54181ae
3bowhab <ali.abdelwhab21@gmail.com>
2025-12-29 18:04:55 +0200

    Add asset image constants and update pubspec for asset management

---
+++ lib/helper/app_images.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesBranding
+  /// assets/images/branding.png
+  static const String imagesBranding = "assets/images/branding.png";
+
+  /// Assets for imagesIslami
+  /// assets/images/islami.png
+  static const String imagesIslami = "assets/images/islami.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesOnboarding4
+  /// assets/images/onboarding4.png
+  static const String imagesOnboarding4 = "assets/images/onboarding4.png";
+
+  /// Assets for imagesOnboarding5
+  /// assets/images/onboarding5.png
+  static const String imagesOnboarding5 = "assets/images/onboarding5.png";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+}
+
