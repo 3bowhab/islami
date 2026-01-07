3e78992f8210ccece92d9ca16399196bb135c4fc
3bowhab <ali.abdelwhab21@gmail.com>
2026-01-07 23:47:38 +0200

    make bottomNavigationBar

--- lib/helper/app_images.dart
+++ lib/helper/app_images.dart
@@ -2,6 +2,142 @@
 class Assets {
   Assets._();
   
+  /// Assets for fontsJannaLTBold
+  /// assets/fonts/Janna_LT_Bold.ttf
+  static const String fontsJannaLTBold = "assets/fonts/Janna_LT_Bold.ttf";
+
+  /// Assets for icons2xHadith
+  /// assets/icons/2x/hadith.png
+  static const String icons2xHadith = "assets/icons/2x/hadith.png";
+
+  /// Assets for icons2xQuran
+  /// assets/icons/2x/quran.png
+  static const String icons2xQuran = "assets/icons/2x/quran.png";
+
+  /// Assets for icons2xRadio
+  /// assets/icons/2x/radio.png
+  static const String icons2xRadio = "assets/icons/2x/radio.png";
+
+  /// Assets for icons2xSebha
+  /// assets/icons/2x/sebha.png
+  static const String icons2xSebha = "assets/icons/2x/sebha.png";
+
+  /// Assets for icons2xTime
+  /// assets/icons/2x/time.png
+  static const String icons2xTime = "assets/icons/2x/time.png";
+
+  /// Assets for icons3xHadith
+  /// assets/icons/3x/hadith.png
+  static const String icons3xHadith = "assets/icons/3x/hadith.png";
+
+  /// Assets for icons3xQuran
+  /// assets/icons/3x/quran.png
+  static const String icons3xQuran = "assets/icons/3x/quran.png";
+
+  /// Assets for icons3xRadio
+  /// assets/icons/3x/radio.png
+  static const String icons3xRadio = "assets/icons/3x/radio.png";
+
+  /// Assets for icons3xSebha
+  /// assets/icons/3x/sebha.png
+  static const String icons3xSebha = "assets/icons/3x/sebha.png";
+
+  /// Assets for icons3xTime
+  /// assets/icons/3x/time.png
+  static const String icons3xTime = "assets/icons/3x/time.png";
+
+  /// Assets for iconsHadith
+  /// assets/icons/hadith.png
+  static const String iconsHadith = "assets/icons/hadith.png";
+
+  /// Assets for iconsQuran
+  /// assets/icons/quran.png
+  static const String iconsQuran = "assets/icons/quran.png";
+
+  /// Assets for iconsRadio
+  /// assets/icons/radio.png
+  static const String iconsRadio = "assets/icons/radio.png";
+
+  /// Assets for iconsSebha
+  /// assets/icons/sebha.png
+  static const String iconsSebha = "assets/icons/sebha.png";
+
+  /// Assets for iconsTime
+  /// assets/icons/time.png
+  static const String iconsTime = "assets/icons/time.png";
+
+  /// Assets for images2xBackground
+  /// assets/images/2x/Background.png
+  static const String images2xBackground = "assets/images/2x/Background.png";
+
+  /// Assets for images2xBranding
+  /// assets/images/2x/branding.png
+  static const String images2xBranding = "assets/images/2x/branding.png";
+
+  /// Assets for images2xIslami
+  /// assets/images/2x/islami.png
+  static const String images2xIslami = "assets/images/2x/islami.png";
+
+  /// Assets for images2xOnboarding1
+  /// assets/images/2x/onboarding1.png
+  static const String images2xOnboarding1 = "assets/images/2x/onboarding1.png";
+
+  /// Assets for images2xOnboarding2
+  /// assets/images/2x/onboarding2.png
+  static const String images2xOnboarding2 = "assets/images/2x/onboarding2.png";
+
+  /// Assets for images2xOnboarding3
+  /// assets/images/2x/onboarding3.png
+  static const String images2xOnboarding3 = "assets/images/2x/onboarding3.png";
+
+  /// Assets for images2xOnboarding4
+  /// assets/images/2x/onboarding4.png
+  static const String images2xOnboarding4 = "assets/images/2x/onboarding4.png";
+
+  /// Assets for images2xOnboarding5
+  /// assets/images/2x/onboarding5.png
+  static const String images2xOnboarding5 = "assets/images/2x/onboarding5.png";
+
+  /// Assets for images2xSplash
+  /// assets/images/2x/splash.png
+  static const String images2xSplash = "assets/images/2x/splash.png";
+
+  /// Assets for images3xBackground
+  /// assets/images/3x/Background.png
+  static const String images3xBackground = "assets/images/3x/Background.png";
+
+  /// Assets for images3xBranding
+  /// assets/images/3x/branding.png
+  static const String images3xBranding = "assets/images/3x/branding.png";
+
+  /// Assets for images3xIslami
+  /// assets/images/3x/islami.png
+  static const String images3xIslami = "assets/images/3x/islami.png";
+
+  /// Assets for images3xOnboarding1
+  /// assets/images/3x/onboarding1.png
+  static const String images3xOnboarding1 = "assets/images/3x/onboarding1.png";
+
+  /// Assets for images3xOnboarding2
+  /// assets/images/3x/onboarding2.png
+  static const String images3xOnboarding2 = "assets/images/3x/onboarding2.png";
+
+  /// Assets for images3xOnboarding3
+  /// assets/images/3x/onboarding3.png
+  static const String images3xOnboarding3 = "assets/images/3x/onboarding3.png";
+
+  /// Assets for images3xOnboarding4
+  /// assets/images/3x/onboarding4.png
+  static const String images3xOnboarding4 = "assets/images/3x/onboarding4.png";
+
+  /// Assets for images3xOnboarding5
+  /// assets/images/3x/onboarding5.png
+  static const String images3xOnboarding5 = "assets/images/3x/onboarding5.png";
+
+  /// Assets for images3xSplash
+  /// assets/images/3x/splash.png
+  static const String images3xSplash = "assets/images/3x/splash.png";
+
   /// Assets for imagesBackground
   /// assets/images/Background.png
   static const String imagesBackground = "assets/images/Background.png";
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,28 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:islami/helper/app_images.dart';
+import 'package:islami/helper/app_colors.dart';
+import 'package:islami/helper/app_lists.dart';
+import 'package:islami/helper/app_text_styles.dart';
+import 'package:islami/model/tabs_model.dart';
+import 'package:islami/widgets/custom_navigation_des.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int selectedIndex = 0;
+  final List<TabsModel> tabs = AppLists().tabs;
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Image.asset(
-          Assets.imagesBackground,
+          tabs[selectedIndex].backgroudImage,
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
         ),
         Scaffold(
           backgroundColor: Colors.transparent,
-          body: const Center(
-            child: Text(
-              'Welcome to the Islami App!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+          bottomNavigationBar: NavigationBar(
+            backgroundColor: AppColors.gold,
+            indicatorColor: AppColors.black.withValues(alpha: .6),
+            labelTextStyle: WidgetStateProperty.all(AppTextStyles.whiteBold(12)),
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            selectedIndex: selectedIndex,
+            destinations: List.generate(
+              tabs.length,
+              (index) => CustomNavigationDes(
+                iconPath: tabs[index].iconpath,
+                label: tabs[index].label,
               ),
             ),
           ),
