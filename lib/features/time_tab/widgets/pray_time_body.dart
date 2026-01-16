d18c5079b98448748a5c2a4899481182dbf2288a
3bowhab <ali.abdelwhab21@gmail.com>
2026-01-16 23:46:44 +0200

    feat: Refactor PrayTimeBody to use PrayTimeBottom widget and simplify state management

--- lib/features/time_tab/widgets/pray_time_body.dart
+++ lib/features/time_tab/widgets/pray_time_body.dart
@@ -1,47 +1,14 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:islami/core/theme/app_colors.dart';
-import 'package:islami/core/theme/app_text_styles.dart';
 import 'package:islami/core/utils/app_utils.dart';
-import 'package:islami/features/time_tab/logic/get_next_prayer.dart';
 import 'package:islami/features/time_tab/widgets/card_slider.dart';
+import 'package:islami/features/time_tab/widgets/pray_time_bottom.dart';
 
-class PrayTimeBody extends StatefulWidget {
+class PrayTimeBody extends StatelessWidget {
   const PrayTimeBody({super.key});
 
   @override
-  State<PrayTimeBody> createState() => _PrayTimeBodyState();
-}
-
-class _PrayTimeBodyState extends State<PrayTimeBody> {
-  Timer? timer;
-  Map<String, dynamic>? nextPrayer;
-
-  @override
-  void initState() {
-    super.initState();
-    updateNextPrayer();
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      updateNextPrayer();
-    });
-  }
-
-  void updateNextPrayer() {
-    setState(() {
-      nextPrayer = getNextPrayer();
-    });
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final Duration remaining = nextPrayer!['remaining'];
-
     return Expanded(
       child: Container(
         width: double.infinity,
@@ -51,24 +18,13 @@ class _PrayTimeBodyState extends State<PrayTimeBody> {
         ),
         child: Padding(
           padding: EdgeInsets.symmetric(
-            vertical: AppUtils.height(context) * (25 / 932),
+            vertical: AppUtils.height(context) * (15 / 932),
           ),
           child: Column(
-            spacing: AppUtils.height(context) * (8 / 932),
+            spacing: AppUtils.height(context) * (25 / 932),
             children: [
               const CardsSlider(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Next Pray - ', style: AppTextStyles.lightBlackBold(16)),
-                  Text(
-                    '${remaining.inHours.toString().padLeft(2, '0')}:'
-                    '${remaining.inMinutes.remainder(60).toString().padLeft(2, '0')}:'
-                    '${remaining.inSeconds.remainder(60).toString().padLeft(2, '0')}',
-                    style: AppTextStyles.blackBold(16),
-                  ),
-                ],
-              ),
+              const PrayTimeBottom(),
             ],
           ),
         ),
---
+++ lib/features/time_tab/widgets/pray_time_bottom.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:islami/core/constants/app_images.dart';
+import 'package:islami/core/theme/app_colors.dart';
+import 'package:islami/core/theme/app_text_styles.dart';
+import 'package:islami/core/utils/app_utils.dart';
+import 'package:islami/features/time_tab/logic/get_next_prayer.dart';
+
+class PrayTimeBottom extends StatefulWidget {
+  const PrayTimeBottom({super.key});
+
+  @override
+  State<PrayTimeBottom> createState() => _PrayTimeBottomState();
+}
+
+class _PrayTimeBottomState extends State<PrayTimeBottom> {
+  Duration get remaining => nextPrayer!['remaining'];
+  Timer? timer;
+  Map<String, dynamic>? nextPrayer;
+  bool isMuted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    updateNextPrayer();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      updateNextPrayer();
+    });
+  }
+
+  void updateNextPrayer() {
+    setState(() {
+      nextPrayer = getNextPrayer();
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        Row(
+          mainAxisAlignment: .center,
+          children: [
+            Text('Next Pray - ', style: AppTextStyles.lightBlackBold(16)),
+            Text(
+              '${remaining.inHours.toString().padLeft(2, '0')}:'
+              '${remaining.inMinutes.remainder(60).toString().padLeft(2, '0')}:'
+              '${remaining.inSeconds.remainder(60).toString().padLeft(2, '0')}',
+              style: AppTextStyles.blackBold(16),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: AppUtils.width(context) * (30 / 430)),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                isMuted = !isMuted;
+              });
+            },
+            child: ImageIcon(
+              AssetImage(
+                isMuted ? Assets.iconsVolumeCross : Assets.iconsVolumeHigh,
+              ),
+              size: 25,
+              color: AppColors.black,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
