5d9a351ed742ced6ed1d4dd8339f709ccce91874
3bowhab <ali.abdelwhab21@gmail.com>
2026-01-19 00:38:34 +0200

    feat: refactor Quran tab layout to use CustomScrollView and SliverList for improved performance and structure

--- lib/features/quran_tab/widgets/suras_list.dart
+++ lib/features/quran_tab/widgets/suras_list.dart
@@ -5,22 +5,35 @@ import 'package:islami/data/models/sura_model.dart';
 import 'package:islami/features/quran_tab/widgets/sura_list_tile.dart';
 
 class SurasList extends StatelessWidget {
-  const SurasList({super.key, required this.filterList, required this.onReturn});
+  const SurasList({
+    super.key,
+    required this.filterList,
+    required this.onReturn,
+  });
+
   final List<SuraModel> filterList;
   final VoidCallback onReturn;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.separated(
-        itemBuilder: (context, index) => SuraListTile(index: index, filterList: filterList, onReturn: onReturn),
-        separatorBuilder: (context, index) => Divider(
-          color: AppColors.white,
-          indent: AppUtils.width(context) * 0.12,
-          endIndent: AppUtils.width(context) * 0.12,
-        ),
-        itemCount: filterList.length,
-      ),
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return Column(
+          children: [
+            SuraListTile(
+              index: index,
+              filterList: filterList,
+              onReturn: onReturn,
+            ),
+            Divider(
+              color: AppColors.white,
+              indent: AppUtils.width(context) * 0.12,
+              endIndent: AppUtils.width(context) * 0.12,
+            ),
+          ],
+        );
+      }, childCount: filterList.length),
     );
   }
 }
+
